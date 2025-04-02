8680b63f9dbe589f3ff2195f9895f85704e0f967
shivamshar03 <shivamshar0310@gmail.com>
2025-04-02 15:44:33 +0530

    add social links

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -53,11 +53,24 @@ const About = () => {
               width="240"
               height="320"
               src="https://www.youtube.com/embed/pSbDSNWQSEk?autoplay=1&mute=1"
-              title="YouTube video"
+              title="NexHub Introduction Video"
               frameBorder="1"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
               allowFullScreen
             ></iframe>
+            <div className="mt-4 text-center">
+              <a 
+                href="https://www.youtube.com/channel/UCUHmRuQ9ELLTC9lVeTtEQpw" 
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-primary hover:text-primary-dark dark:text-primary-light dark:hover:text-primary inline-flex items-center"
+              >
+                Visit our YouTube channel
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 ml-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14" />
+                </svg>
+              </a>
+            </div>
           </motion.div>
         </div>
 
